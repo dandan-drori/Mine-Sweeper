a1aaa2cbecab933a839181b16bf62354c881bd23
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2021-05-20 16:54:08 +0300

    Add Hint and Safe Click

--- js/main.js
+++ js/main.js
@@ -3,18 +3,14 @@ const EMPTY = ''
 const FLAG = '<img src="img/flag.png" alt="flag">'
 
 var gBoard
-// Cell:
-// minesAround: 4
-//isShown: true
-//isMine: false
-//isMarked: true
 var gLevel
 var gGame
 var gIsFirstClick
 var gTimeInterval
+var gIsUsingHint
+var gIsManualMode
 
 function init() {
-	clearInterval(gTimeInterval)
 	handleReRender(4, 2)
 }
 
@@ -82,13 +78,32 @@ function countNeighbors(rowIdx, colIdx) {
 }
 
 function cellClicked(elCell, i, j) {
-	if (gIsFirstClick) {
+	if (!gGame.isOn) return
+	var cell = gBoard[i][j]
+	if (cell.isShown) return
+	console.log('gIsManualMode', gIsManualMode)
+	console.log('gLevel.PLACED_MINES', gLevel.PLACED_MINES)
+	console.log('gLevel.MINES', gLevel.MINES)
+	if (gIsManualMode && gLevel.PLACED_MINES < gLevel.MINES) {
+		debugger
+		useManualMode(elCell, i, j)
+		gLevel.PLACED_MINES++
+		return
+	}
+	if (gIsManualMode && gLevel.PLACED_MINES === gLevel.MINES) {
+		startTime()
+		setMinesNegsCount()
+		gIsManualMode = false
+	}
+	if (gIsFirstClick && !gIsManualMode) {
 		startTime()
 		placeMines()
 		setMinesNegsCount()
 	}
-	if (!gGame.isOn) return
-	var cell = gBoard[i][j]
+	if (gIsUsingHint) {
+		useHint(i, j)
+		return
+	}
 	if (cell.isMine) mineClicked(elCell, cell)
 	if (cell.isMarked) return
 	if (!cell.minesAround && !cell.isMine) expandShown(i, j)
@@ -111,7 +126,24 @@ function renderCell(elCell, cell) {
 			elCell.style.backgroundColor = '#888'
 		} else if (cell.minesAround) {
 			strHtml = cell.minesAround
+			elCell.style.backgroundColor = '#888'
+			switch (cell.minesAround) {
+				case 1:
+					elCell.style.color = 'blue'
+					break
+				case 2:
+					elCell.style.color = 'green'
+					break
+				case 3:
+					elCell.style.color = 'red'
+					break
+				case 4:
+					elCell.style.color = 'black'
+					break
+			}
 		}
+	} else {
+		elCell.style.backgroundColor = '#aaa'
 	}
 	elCell.innerHTML = strHtml
 }
@@ -156,11 +188,12 @@ function gameOver(isWin) {
 	if (isWin) {
 		gGame.isOn = false
 		elRestartBtn.innerText = '😎'
+		showWinGif()
 	} else {
 		showMines()
 		gGame.isOn = false
 		elRestartBtn.innerText = '🤕'
-		// '☹️'
+		document.querySelector('.lives span').innerHTML = '💀'
 	}
 	handleLocalStorage()
 }
@@ -197,7 +230,7 @@ function isVictory() {
 			var cell = gBoard[i][j]
 			// all the mines are flagged,and all the other cells are shown
 			// if theres a mine that is not flagged, or a cell that is not shown
-			// FIXME: if (cell.isMine && !cell.isMarked) return false
+			if (cell.isMine && !cell.isMarked && !cell.isShown) return false
 			if (!cell.isShown && !cell.isMine) return false
 		}
 	}
@@ -220,6 +253,7 @@ function handleReRender(size, mines) {
 		SIZE: size,
 		MINES: mines,
 		HIGHSCORE: highscore,
+		PLACED_MINES: 0,
 	}
 	gGame = {
 		isOn: true,
@@ -228,22 +262,36 @@ function handleReRender(size, mines) {
 		secsPassed: 0,
 		lives: 3,
 		score: 0,
+		hints: 3,
+		safeClicks: 3,
 	}
 	clearInterval(gTimeInterval)
 	gIsFirstClick = true
+	gIsUsingHint = false
 	gBoard = buildBoard(gLevel.SIZE)
 	renderBoard(gBoard)
-	var elRestartBtn = document.querySelector('.restart-btn')
-	elRestartBtn.innerText = '😃'
+
+	document.querySelector('.restart-btn').innerText = '😃'
+
 	var elLives = document.querySelector('.lives span')
 	var strHtml = ''
 	for (var i = 0; i < gGame.lives; i++) {
 		strHtml += '❤️ '
 	}
 	elLives.innerHTML = strHtml
+
 	var elScore = document.querySelector('.score span')
 	elScore.innerText = gGame.score
+
 	document.querySelector('.highscore span').innerText = gLevel.HIGHSCORE
+
+	hideWinGif()
+
+	var elLightbulbs = document.querySelectorAll('.lightbulb')
+	for (var i = 0; i < elLightbulbs.length; i++) {
+		elLightbulbs[i].src = 'img/lightbulb.png'
+		elLightbulbs[i].setAttribute('onclick', `activateHint(${i + 1})`)
+	}
 }
 
 function mineClicked(elCell, cell) {
@@ -253,11 +301,13 @@ function mineClicked(elCell, cell) {
 	for (var i = 0; i < gGame.lives; i++) {
 		strHtml += '❤️ '
 	}
+
 	elLives.innerHTML = strHtml
 	if (!gGame.lives) {
 		gameOver(false)
 		return
 	}
+
 	elCell.style.backgroundColor = 'red'
 	cell.isShown = true
 	renderCell(elCell, cell)
@@ -274,6 +324,7 @@ function updateScore(diff) {
 
 function placeMines() {
 	var minesCount = 0
+
 	while (minesCount < gLevel.MINES) {
 		var randRow = getRandomInt(0, gLevel.SIZE)
 		var randCol = getRandomInt(0, gLevel.SIZE)
@@ -290,6 +341,7 @@ function placeMines() {
 
 function handleLocalStorage() {
 	var highscores = localStorage.getItem('highscores')
+
 	if (!highscores) {
 		var baseHighscores = {
 			size4: 0,
@@ -299,9 +351,11 @@ function handleLocalStorage() {
 		baseHighscores[`size${gLevel.SIZE}`] = gGame.score
 		var jsonBase = JSON.stringify(baseHighscores)
 		localStorage.setItem('highscores', jsonBase)
+
 		return
 	}
 	var parsedHighscores = JSON.parse(highscores)
+
 	if (gGame.score > parsedHighscores[`size${gLevel.SIZE}`]) {
 		parsedHighscores[`size${gLevel.SIZE}`] = gGame.score
 		var jsonHighscores = JSON.stringify(parsedHighscores)
@@ -312,7 +366,144 @@ function handleLocalStorage() {
 function getLevelHighscore(size) {
 	var highscores = localStorage.getItem('highscores')
 	if (!highscores) return 0
+
 	var parsedHighscores = JSON.parse(highscores)
 	var highscore = parsedHighscores[`size${size}`]
 	return highscore
+}
+
+function showWinGif() {
+	elWinModal = document.querySelector('.win-modal')
+	elWinModal.style.display = 'block'
+
+	setTimeout(hideWinGif, 5000)
+}
+
+function hideWinGif() {
+	elWinModal = document.querySelector('.win-modal')
+	elWinModal.style.display = 'none'
+}
+
+// when a cell is clicked using hint
+function useHint(rowIdx, colIdx) {
+	var isCellsShown = saveIsShown(rowIdx, colIdx)
+	toggleCells(rowIdx, colIdx, true, isCellsShown)
+
+	setTimeout(() => {
+		toggleCells(rowIdx, colIdx, false, isCellsShown)
+		gIsUsingHint = false
+	}, 1000)
+}
+
+function toggleCells(rowIdx, colIdx, isToShow, isShownArr) {
+	// the cell and its neighbors are revealed for *1 second*:
+	var idx = 0
+
+	for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+		if (i < 0 || i > gBoard.length - 1) continue
+		for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+			if (j < 0 || j > gBoard[0].length - 1) continue
+			// reveal the cell and it's neighbors
+			var cell = gBoard[i][j]
+			cell.isShown = isToShow ? true : isShownArr[idx++]
+			var className = getClassName(i, j)
+			var elCell = document.querySelector(`.${className}`)
+			renderCell(elCell, cell)
+		}
+	}
+}
+
+// when a lightbulb is clicked
+function activateHint(imgNum) {
+	// decrease number of hints left
+	gGame.hints--
+
+	// show indication that the bulb was clicked
+	var className = `.lightbulb${imgNum}`
+	var elImg = document.querySelector(className)
+	elImg.src = 'img/darkbulb.png'
+	elImg.style.cursor = 'default'
+
+	// make the next click do something else:
+	gIsUsingHint = true
+
+	// make sure the lightbulb is not clickable anymore
+	elImg.onclick = ''
+}
+
+function activateSafeClick() {
+	if (gGame.safeClicks === 0) return
+	// decrease safe click count in gGame
+	gGame.safeClicks--
+	// render the update safe click count on the block
+	document.querySelector('.remaining').innerText = gGame.safeClicks
+	// find all the non-mines cells
+	var safeCells = []
+	for (var i = 0; i < gBoard.length; i++) {
+		for (var j = 0; j < gBoard[0].length; j++) {
+			var cell = gBoard[i][j]
+			if (!cell.isMine && !cell.isShown) {
+				safeCells.push({ i, j })
+			}
+		}
+	}
+	// select a random cell from that array
+	var randIdx = getRandomInt(0, safeCells.length)
+	var randCell = safeCells[randIdx]
+	var className = getClassName(randCell.i, randCell.j)
+	var elCell = document.querySelector(`.${className}`)
+	// make it blink
+	elCell.classList.add('blink')
+
+	setTimeout(() => elCell.classList.remove('blink'), 1005)
+}
+
+function useManualMode(elCell, i, j) {
+	// the user first clicks on cells to place the mines there
+	var cell = gBoard[i][j]
+	cell.isMine = true
+	cell.isShown = true
+	renderCell(elCell, cell)
+
+	setTimeout(() => {
+		cell.isShown = false
+		renderCell(elCell, cell)
+	}, 1000)
+}
+
+function activateManualMode() {
+	gIsManualMode = true
+}
+
+function saveIsShown(rowIdx, colIdx) {
+	var isCellsShown = []
+
+	for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+		if (i < 0 || i > gBoard.length - 1) continue
+		for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+			if (j < 0 || j > gBoard[0].length - 1) continue
+			var cell = gBoard[i][j]
+			isCellsShown.push(cell.isShown)
+		}
+	}
+
+	return isCellsShown
+}
+
+function undo() {
+	// undo cell properties
+	// re-render cell
+
+	// undo lives
+	// undo
+	gGame = {
+		isOn: true,
+		shownCount: 0,
+		markedCount: 0,
+		secsPassed: 0,
+		lives: 3,
+		score: 0,
+		hints: 3,
+		safeClicks: 3,
+	}
 }
